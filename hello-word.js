04edb4e1885506cb66f5eb34ae605c0a89d26922
lenglerweb <lenglerweb@gmail.com>
2020-04-09 15:24:51 -0300

    pc Stats

---
+++ hello-word.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Curso de Javascript - Rocketseat</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<script type="text/javascript">
+		console.log("Hello Word!");
+		console.log("ou Olá Mundo!");
+	</script>
+</body>
+</html>
